55abc90639f0c24188dc9b3f0029081ce9626f9d
pavlo.hrishyn <pashagrishyn@gmai.com>
2022-01-03 22:51:42 +0200

    added core project, added processor and interface for strategy

---
+++ src/CCTradingPlatform.Core/ITradingStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCTradingPlatform.Core
+{
+    public interface ITradingStrategy
+    {
+        void Execute();
+    }
+}
---
+++ src/CCTradingPlatform.Core/TradingProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CCTradingPlatform.Core
+{
+    public class TradingProcessor
+    {
+        private readonly decimal _upperLimit;
+        private readonly Channel<decimal> _currentPriceChan;
+
+        public TradingProcessor(decimal upperLimit, Channel<decimal> currentPriceChan)
+        {
+            _upperLimit = upperLimit;
+            _currentPriceChan = currentPriceChan ?? throw new ArgumentNullException(nameof(currentPriceChan));
+        }
+
+        public async Task Start(ITradingStrategy strategy)
+        {
+            strategy.Execute();
+
+        }
+    }
+}
